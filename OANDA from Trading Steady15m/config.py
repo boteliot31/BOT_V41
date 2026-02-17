f42a1c5ffa7277d2a1c04b475bb016c8afa1f3c2
boteliot31 <109971800+boteliot31@users.noreply.github.com>
2026-02-17 15:36:32 +0100

    Update config.py Effacer API Keys

--- OANDA from Trading Steady15m/config.py
+++ OANDA from Trading Steady15m/config.py
@@ -7,8 +7,8 @@ WHITH  -  BINANCE -- ..
 ### import pandas_ta as ta
 ### import numpy as np
 # LE SCREENER  2 ........................................................
-BINANCE_API_KEY ='GwvK4xNzrBr1HpEozMWx2AnWevMRlUZxPgroPvDcy2l4tqdzBH1eBa6PSpriyQTb'
-BINANCE_API_SECRET = 'DGij2y3b9teZHM0vrqafkGzuKbtKCM4wtI3RpA96DqPFNo433HCUJhs4cnSI3MQq'
+BINANCE_API_KEY ='Gw6PSpriyQTb'
+BINANCE_API_SECRET = 'DGij2s4cnSI3MQq'
 ### client = Client(api_key,api_secret)
 #client = Client()
 ### client.get_account()  # <<< Pour avoir le solde du compte BINANACE   ...
